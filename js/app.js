71bfb54f6599bdafc46bd6078f85c099b00bce69
rippler7 <darkse7enth@gmail.com>
2018-01-12 09:16:13 +0800

    1/12/2018 9:16AM

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 console.log("start");
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  seconds = seconds < 10 ? '0'+seconds : seconds;
+  var strTime = hours + ':' + minutes + ':' + seconds +' '+ ampm;
+  return strTime;
+}
+
 $(document).ready(function(){
 	console.log("ready");
     $("ul#ticker01").liScroll();
+
+    var monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var date = new Date();
+    var dd = date.getDate();
+    var mm = date.getMonth();
+    var yyyy = date.getFullYear();
+
+    console.log(monthNames[mm]+"/"+dd+"/"+yyyy);
+
+    $("#mnlTime").html(formatAMPM(date).toString());
+
 });
