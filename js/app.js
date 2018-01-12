1016869fb032e060811415883ef34bd6fe699c44
rippler7 <darkse7enth@gmail.com>
2018-01-12 10:54:03 +0800

    1/12/2018 10:53AM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 console.log("start");
 
 function formatAMPM(date) {
@@ -13,20 +14,40 @@ function formatAMPM(date) {
   return strTime;
 }
 
-$(document).ready(function(){
-	console.log("ready");
-    $("ul#ticker01").liScroll();
+function toTimeZone(time, zone) {
+    var format = 'hh:mm:ss A';
+    return moment(time, format).tz(zone).format(format);
+}
 
-    var monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+function liveTime(){
+	var monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
     var date = new Date();
     var dd = date.getDate();
     var mm = date.getMonth();
     var yyyy = date.getFullYear();
 
-    console.log(monthNames[mm]+"/"+dd+"/"+yyyy);
+    var timestamp = date.getTime();
+
     $("#bigDate").html(dd);
     $("#bigMonth").html(monthNames[mm]);
     $("#bigYear").html(yyyy);
-    $("#mnlTime").html(formatAMPM(date).toString());
+  
+    var tz = moment.tz.guess();
+    var timeManila = toTimeZone(date,tz);
+    var timeEST = toTimeZone(date,"America/New_York");
+    var timePST = toTimeZone(date,"America/Los_Angeles");
+    var timeGMT = toTimeZone(date,"Etc/GMT+0");
+    var timeAEST = toTimeZone(date,"Australia/Melbourne");
 
+    $("#mnlTime").html(timeManila);
+    $("#estTime").html(timeEST);
+    $("#pstTime").html(timePST);
+    $("#gmtTime").html(timeGMT);
+    $("#aestTime").html(timeAEST);
+}
+
+$(document).ready(function(){
+	console.log("ready");
+    $("ul#ticker01").liScroll();
+    var liveCount = setInterval(liveTime,1000);
 });
